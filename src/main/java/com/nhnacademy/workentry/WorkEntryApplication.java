2bee4d72ad08adf5be82a78c70d786967586e181
rudduddl <kim5472678@gmail.com>
2025-04-18 16:55:02 +0900

    build : 기본 환경 설정

---
+++ src/main/java/com/nhnacademy/workentry/WorkEntryApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.workentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkEntryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkEntryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/workentry/WorkEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.workentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkEntryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
