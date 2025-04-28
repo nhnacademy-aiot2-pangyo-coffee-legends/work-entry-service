279af2f99a2bb64ce195a15b2ac6fe726e7bc036
Migong0311 <kimms000311@gmail.com>
2025-04-28 15:24:07 +0900

    refector : mbNo->no 수정

--- src/main/java/com/nhnacademy/workentry/attendance/service/AttendanceService.java
+++ src/main/java/com/nhnacademy/workentry/attendance/service/AttendanceService.java
@@ -1,7 +1,6 @@
-package attendance.service;
+package com.nhnacademy.workentry.attendance.service;
 
-import attendance.dto.AttendanceDto;
-import com.nhnacademy.frontservice.attendance.dto.AttendanceResponse;
+import com.nhnacademy.workentry.attendance.dto.AttendanceDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,20 +10,20 @@ public interface AttendanceService {
     /**
      * 특정 회원의 전체 출결 내역 조회
      *
-     * @param mbNo 회원 번호
+     * @param no 회원 번호
      * @return 출결 응답 DTO 리스트
      */
-    List<AttendanceDto> getAttendanceByMbNo(Long mbNo);
+    List<AttendanceDto> getAttendanceByNo(Long no);
 
     /**
      * 특정 회원의 기간별 출결 기록을 조회합니다.
      *
-     * @param mbNo 회원 번호
+     * @param no 회원 번호
      * @param start 시작 날짜
      * @param end 종료 날짜
      * @return 출결 DTO 목록
      */
-    List<AttendanceDto> getAttendanceByMbNoAndDateRange(Long mbNo, LocalDateTime start, LocalDateTime end);
+    List<AttendanceDto> getAttendanceByNoAndDateRange(Long no, LocalDateTime start, LocalDateTime end);
 
     /**
      * 전체 회원의 최근 7일간 출결 현황을 요약합니다.
