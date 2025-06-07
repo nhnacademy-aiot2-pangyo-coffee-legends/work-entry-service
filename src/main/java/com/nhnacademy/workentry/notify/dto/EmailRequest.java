1a9ba395f263cff36a102ef60c285ae80b3a776d
rudduddl <kim5472678@gmail.com>
2025-06-07 19:12:23 +0900

    feat : roleType 변수 추가

--- src/main/java/com/nhnacademy/workentry/notify/dto/EmailRequest.java
+++ src/main/java/com/nhnacademy/workentry/notify/dto/EmailRequest.java
@@ -2,16 +2,24 @@ package com.nhnacademy.workentry.notify.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class EmailRequest implements Serializable {
+
     private String to;
     private String subject;
     private String content;
+    private String roleType; // "ROLE_ADMIN" 또는 "ROLE_ALL"
     private String type; // "TEXT" 또는 "HTML"
+
+    public EmailRequest(String to, String subject, String content, String roleType) {
+        this.to = to;
+        this.subject = subject;
+        this.content = content;
+        this.roleType = roleType;
+        this.type = null;
+    }
 }
