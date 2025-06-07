4d7509d2ec9b183813c11ee1e26537b152ae070a
rudduddl <kim5472678@gmail.com>
2025-06-07 19:01:35 +0900

    feat : EmailRequest 어노테이션 수정

--- src/main/java/com/nhnacademy/workentry/notify/dto/EmailRequest.java
+++ src/main/java/com/nhnacademy/workentry/notify/dto/EmailRequest.java
@@ -2,22 +2,16 @@ package com.nhnacademy.workentry.notify.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class EmailRequest implements Serializable {
-
     private String to;
     private String subject;
     private String content;
     private String type; // "TEXT" 또는 "HTML"
-
-    public EmailRequest(String to, String subject, String content, String type) {
-        this.to = to;
-        this.subject = subject;
-        this.content = content;
-        this.type = type;
-    }
 }
